2500c7d90962e63ec80f01c0884c1ea0aa837cb2
Natalia Serebrennikova <inkinyam@yandex.ru>
2023-03-29 10:53:10 +0300

    fix: общая страница модерации идей

---
+++ scripts/moderation-control.js
@@ -0,0 +1,18 @@
+let cardsArray = Array.from(document.querySelectorAll('.moderation__tables-item'));
+
+if (cardsArray.length != 0) {
+  cardsArray.map(item => {
+    let aproveButton = item.querySelector('.aprove');
+    let declineButton = item.querySelector('.decline');
+
+    aproveButton.addEventListener('click', ()=> {
+      item.classList.remove('declined');
+      item.classList.toggle('approved');
+    })
+
+    declineButton.addEventListener('click', ()=> {
+      item.classList.remove('approved');
+      item.classList.toggle('declined');
+    })
+  })
+}
